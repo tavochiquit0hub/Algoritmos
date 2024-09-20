90708f53de0e15e4c99684b425ae7e001101fa47
tavochiquit0 <l24200223@pachuca.tecnm.mx>
2024-09-20 12:59:59 -0600

    prueba

---
+++ strDataType.py
@@ -0,0 +1,9 @@
+x = "Hello World"
+#para saber que tipo de dato es necesario
+#poner su tipo de dato antes
+#ejem x=str("Hello World")
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
